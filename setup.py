5507f7368e38683c11de19700ac8cad1bc1f69c9
marcel <marcel.nicolay@gmail.com>
2012-09-21 15:56:51 -0300

    fix requires and distribute

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 setup(
     name='django-shipping',
     version='0.0.1',
@@ -8,7 +7,7 @@ setup(
     long_description=open("README.md").read(),
     author=u'Marcel Nicolay',
     author_email='marcelnicolay@gmail.com',
-    url='http://github.com/marcelnicolay/django-shipping',
+    url='http://github.com/quatix/django-shipping',
     install_requires=open("requirements.txt").read().split("\n"),
     packages=['shipping'],
 )
