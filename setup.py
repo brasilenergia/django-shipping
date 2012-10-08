7c8f27a6c9b5ecc2cea4d89111005c0ffd90b90d
marcel <marcel.nicolay@gmail.com>
2012-10-08 16:23:57 -0300

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_packages():
 
 setup(
     name='django-shipping',
-    version='0.0.4',
+    version='0.0.5',
     description='Django shipping tool',
     long_description=open("README.md").read(),
     author=u'Marcel Nicolay',
